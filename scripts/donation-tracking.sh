d741dd8b69dd42c7e2323013f669d22c6b1bb918
Caleb Hayashida <santeyio@gmail.com>
2017-10-20 20:52:49 +0000

    change gunicorn startup to support websockets

--- scripts/donation-tracking.sh
+++ scripts/donation-tracking.sh
@@ -12,5 +12,5 @@ pip install -r requirements.txt &> $DIR/log-donation-tracking.log;
 chmod 755 /var/opt/donation-tracking/donation-tracking &> $DIR/log-donation-tracking.log;
 find /var/opt/donation-tracking/donation-tracking/static -type d -exec chmod 755 {} \ &> $DIR/log-donation-tracking.log;
 find /var/opt/donation-tracking/donation-tracking/static -type f -exec chmod 644 {} \ &> $DIR/log-donation-tracking.log;
-../bin/gunicorn -D --bind 127.0.0.1:5503 -p ../tmp/gunicorn.pid app:app &> $DIR/log-donation-tracking.log;
+../bin/gunicorn -D --worker-class eventlet -w 1 --bind 127.0.0.1:5503 -p ../tmp/gunicorn.pid app:app &> $DIR/log-donation-tracking.log;
 
